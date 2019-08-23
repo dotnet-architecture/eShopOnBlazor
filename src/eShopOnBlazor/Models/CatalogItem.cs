ee7e453a94027aefcc613f0212053f92d297b7b5
Taylor Southwick <tasou@microsoft.com>
2019-08-23 08:49:44 -0700

    React to PR feedback

--- src/eShopOnBlazor/Models/CatalogItem.cs
+++ src/eShopOnBlazor/Models/CatalogItem.cs
@@ -13,6 +13,7 @@ namespace eShopOnBlazor.Models
         }
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "The Name field is required")]
         public string Name { get; set; }
 
         public string Description { get; set; }
